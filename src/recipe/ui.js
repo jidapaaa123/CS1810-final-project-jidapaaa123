18a417503347d80bbd34270d6aa3583071da6e65
Jidapa Angsutti <jidapa.angsutti@students.snow.edu>
2024-04-26 17:37:33 -0600

    working on recipe page: updated design for pendingRecipePage

--- src/recipe/ui.js
+++ src/recipe/ui.js
@@ -1,57 +1,57 @@
-import { GetRecipeById } from "./domain.js";
+// import { GetRecipeById } from "./domain.js";
 
-const mainElement = document.getElementById("main");
+// const mainElement = document.getElementById("main");
 
-const GetId = () => {
-  if (!document.location.search.includes("?")) {
-    return null;
-  }
+// const GetId = () => {
+//   if (!document.location.search.includes("?")) {
+//     return null;
+//   }
 
-  const queryString = document.location.search.split("?")[1];
-  const parts = queryString.split("=");
+//   const queryString = document.location.search.split("?")[1];
+//   const parts = queryString.split("=");
 
-  if (parts[0] != "id" || parts[1].trim() == "") {
-    return null;
-  }
+//   if (parts[0] != "id" || parts[1].trim() == "") {
+//     return null;
+//   }
 
-  return parts[1];
-};
+//   return parts[1];
+// };
 
-const RenderRecipe = async () => {
-  const id = GetId();
-  const recipe = await GetRecipeById(id);
+// const RenderRecipe = async () => {
+//   const id = GetId();
+//   const recipe = await GetRecipeById(id);
 
-  document.title = recipe == null ? "Recipe NOT found" : `Recipe - ${recipe.name}`;
-  if (recipe == null) {
-    mainElement.textContent = `Cannot find recipe with id ${id}`;
-    return;
-  }
+//   document.title = recipe == null ? "Recipe NOT found" : `Recipe - ${recipe.name}`;
+//   if (recipe == null) {
+//     mainElement.textContent = `Cannot find recipe with id ${id}`;
+//     return;
+//   }
 
-  let ingre = "";
+//   let ingre = "";
   
-  recipe.ingredients.forEach(i => {
-    let text = `${i.isOptional ? "@" : ""}${i.name}(`;
+//   recipe.ingredients.forEach(i => {
+//     let text = `${i.isOptional ? "@" : ""}${i.name}(`;
 
-    i.substitutes.forEach(s => {
-        text += s + "/";
-    });
-    text += ")";
-    ingre += text;
-  });
+//     i.substitutes.forEach(s => {
+//         text += s + "/";
+//     });
+//     text += ")";
+//     ingre += text;
+//   });
 
-  console.log(recipe)
-  let text = `NAME: ${recipe.name}
-  <br />
-  hasRequiredInfo: ${recipe.hasRequiredInfo}
-  <br />
-  isPending: ${recipe.isPending}
-  <br />
-  ingredients: ${ingre}
-  <br />
-  INSTRUCTIONS: ${recipe.instructions}`;
+//   console.log(recipe)
+//   let text = `NAME: ${recipe.name}
+//   <br />
+//   hasRequiredInfo: ${recipe.hasRequiredInfo}
+//   <br />
+//   isPending: ${recipe.isPending}
+//   <br />
+//   ingredients: ${ingre}
+//   <br />
+//   INSTRUCTIONS: ${recipe.instructions}`;
 
-  mainElement.innerHTML = text;
+//   mainElement.innerHTML = text;
 
-};
+// };
 
-RenderRecipe();
+// RenderRecipe();
